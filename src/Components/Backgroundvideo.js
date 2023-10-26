83d6b2eb32fe2e205512769278331327366f5cde
Himanshu Sharma <50957983+shaarma12@users.noreply.github.com>
2023-10-26 22:18:38 +0530

    background video changes

--- src/Components/Backgroundvideo.js
+++ src/Components/Backgroundvideo.js
@@ -11,12 +11,12 @@ const Backgroundvideo = ({ movieId }) => {
         src={
           "https://www.youtube.com/embed/" +
           bgTrailer?.key +
-          "?autoplay=1&mute=1"
+          "?autoplay=1&loop=1&autopause=0&mute=1"
         }
         title="YouTube video player"
         allowFullScreen
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-        className="w-[94.974rem]  aspect-video -mt-12"
+        className="w-[94.974rem] aspect-video -mt-12"
       ></iframe>
     </div>
   );
