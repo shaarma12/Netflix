86a3dbf7fe8cd340928eda6b7662f2abd241fe56
Himanshu Sharma <50957983+shaarma12@users.noreply.github.com>
2023-11-01 23:54:47 +0530

    watch Later page created

--- src/Components/WatchLater.js
+++ src/Components/WatchLater.js
@@ -1,12 +1,22 @@
 import React from "react";
 import { useSelector } from "react-redux";
+import Header from "./Header";
+import Moviecard from "./Moviecard";
 
 const WatchLater = () => {
   const cart = useSelector((store) => store.cart.cartArray);
   console.log(cart);
   return (
     <div>
+      <Header />
       <h1>WatchLater</h1>
+      <div className="flex overflow-x-scroll scroll-smooth no-scrollbar">
+        <div className="flex gap-2 pl-14">
+          {cart.map((i) => (
+            <Moviecard key={i.id} movieData={i} />
+          ))}
+        </div>
+      </div>
     </div>
   );
 };
