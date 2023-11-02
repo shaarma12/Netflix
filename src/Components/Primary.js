c8a0838af26df097a627a5d9645241e859b487f5
Himanshu Sharma <50957983+shaarma12@users.noreply.github.com>
2023-11-02 23:10:34 +0530

    watch Later page created

--- src/Components/Primary.js
+++ src/Components/Primary.js
@@ -5,7 +5,7 @@ import { useSelector } from "react-redux";
 const Primary = () => {
   const movies = useSelector((store) => store.movie?.nowPlayingMovies);
   if (movies === null) return;
-  const movie = movies[0];
+  const movie = movies[1];
   const { title, overview, id } = movie;
   return (
     <div>
