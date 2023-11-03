ae7066455f1bafa2225347b3c3f4849d1c38c786
Himanshu Sharma <50957983+shaarma12@users.noreply.github.com>
2023-11-03 23:56:58 +0530

    Movie Card onHover Functionality.

--- src/Components/Moviecard.js
+++ src/Components/Moviecard.js
@@ -1,9 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import { IMG } from "../Utils/constant";
 import { useDispatch } from "react-redux";
 import { addToCart } from "../Utils/addToCartSlice";
 import VideoCard from "./VideoCard";
 const Moviecard = ({ movieData }) => {
+  const [newHeading, setnewheading] = useState(null);
   const dispatch = useDispatch();
   if (!movieData?.poster_path) {
     return null;
@@ -12,7 +13,7 @@ const Moviecard = ({ movieData }) => {
     <div
       className=" w-52 hover:scale-105 transition-all duration-300 drop-shadow-xl cursor-pointer"
       onMouseEnter={() => {
-        <VideoCard movieid={movieData?.id} />;
+        setnewheading(<VideoCard movieid={movieData?.id} />);
       }}
       onClick={() => {
         dispatch(addToCart(movieData));
@@ -21,6 +22,7 @@ const Moviecard = ({ movieData }) => {
       {/* <h1 className="text-white absolute top-16 left-36 text-xl font-bold bg-gradient-to-b from-black">
         {title.slice(0, 10)}
       </h1> */}
+      {newHeading}
       <img src={IMG + movieData?.poster_path} className="rounded-md" />
     </div>
   );
