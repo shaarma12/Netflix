4a41409e8f892d648c484da66b9609acae7116c3
Himanshu Sharma <50957983+shaarma12@users.noreply.github.com>
2023-11-16 23:18:24 +0530

    responsiveness GPT page.

--- src/Components/GPTPage.js
+++ src/Components/GPTPage.js
@@ -5,11 +5,11 @@ import GPTMovies from "./GPTMovies";
 const GPTPage = () => {
   return (
     <div>
-      <div class="fixed">
+      <div class="fixed md:fixed">
         <img
           src="https://assets.nflxext.com/ffe/siteui/vlv3/ab180a27-b661-44d7-a6d9-940cb32f2f4a/7fb62e44-31fd-4e1f-b6ad-0b5c8c2a20ef/IN-en-20231009-popsignuptwoweeks-perspective_alpha_website_large.jpg"
           alt="Your Image"
-          className="h-screen w-screen md:h-full object-cover"
+          className="md:h-full h-full md:object-cover"
         />
         <div class="absolute inset-0 bg-black opacity-50"></div>
       </div>
